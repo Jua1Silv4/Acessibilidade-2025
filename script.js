8fb9edd430e9ce16334333bc52a3e27cce87989d
Jao Fortin <joao.silva.gaspar@escola.pr.gov.br>
2025-03-11 16:16:25 -0300

    11/03

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener ('DOMContentLoaded', function(){
-const aumentaFonteBotao=document.getElementById('aumentar-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
 
-let tamanhoAtualFonte=1;
-aumentaFonteBotao.addEventListener ('click',function(){
-tamanhoAtual +0.1;
-document.body.style.fontSize=`${tamanhoAtualFonte}rem `
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtual + 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem `
 
-})
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtual + 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem `
 
 
 
@@ -15,4 +19,5 @@ document.body.style.fontSize=`${tamanhoAtualFonte}rem `
 
 
 
-})
+
+    })
