addeadd433508af3ec66d12446727580c50da2a1
Jao Fortin <joao.silva.gaspar@escola.pr.gov.br>
2025-03-18 15:52:18 -0300

    18/03

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtual + 0.1;
+        tamanhoAtual +=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem `
 
     })
 
     diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtual + 0.1;
+        tamanhoAtual -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 
