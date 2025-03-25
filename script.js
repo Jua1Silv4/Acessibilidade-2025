1fb79fa6f959c835fdc03f028a1b3c0d4390f9d8
Jao Fortin <joao.silva.gaspar@escola.pr.gov.br>
2025-03-25 14:17:13 -0300

    25/03

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('botao-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -6,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtual +=0.1;
+        tamanhoAtual += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem `
 
     })
 
     diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtual -=0.1;
+        tamanhoAtual -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 
